edbf9dfdc66a312b22ca1944ac74669197ea82be
reconomi <richard.economides@gmail.com>
2016-09-16 13:48:25 +0000

    include test

---
+++ release_mgtTests/StartupTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using release_mgt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace release_mgt.Tests
+{
+    [TestClass()]
+    public class StartupTests
+    {
+        [TestMethod()]
+        public void ConfigurationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ release_mgt_test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace release_mgt_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
